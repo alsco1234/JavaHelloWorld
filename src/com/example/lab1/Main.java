a1a4253dd59333fb5e1d0940d497c72698901628
alsco1234 <22000080@handong.edu>
2021-09-05 00:05:05 +0900

    2st commit

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -6,8 +6,8 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Hello World!!!\n");
-		System.out.print("??? ??? : ");
+		System.out.print("이름 입력 : ");
 		String name = s.next();
-		System.out.println("[" + name + "]?? ???????~");
+		System.out.println("[" + name + "]님 환영합니다~");
 	}
 }
